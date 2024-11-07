2ff6925f3590ff859f29c96bcd2f7b51a7e95a18
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-11-07 08:33:41 +0300

    modules updated

---
+++ java/spring-boot-examples/utility/src/main/java/com/gucardev/utility/UtilityApplication.java
@@ -0,0 +1,16 @@
+package com.gucardev.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@SpringBootApplication
+public class UtilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityApplication.class, args);
+    }
+
+}
