9f3bcadec52fc2d47171ac90779e487080586c8e
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-11-22 20:39:32 +0300

    refactor

--- java/spring-boot-examples/utility/src/main/java/com/gucardev/utility/UtilityApplication.java
+++ java/spring-boot-examples/utility/src/main/java/com/gucardev/utility/UtilityApplication.java
@@ -1,60 +1,18 @@
 package com.gucardev.utility;
 
-import com.gucardev.utility.infrastructure.util.CRLFLogConverter;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.core.env.Environment;
 
 @Slf4j
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
 public class UtilityApplication {
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(UtilityApplication.class);
-        Environment env = app.run(args).getEnvironment();
-        logApplicationStartup(env);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(UtilityApplication.class, args);
+  }
 
-    private static void logApplicationStartup(Environment env) {
-        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
-        String applicationName = env.getProperty("spring.application.name");
-        String serverPort = env.getProperty("server.port");
-        String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path"))
-            .filter(StringUtils::isNotBlank)
-            .orElse("/");
-        String hostAddress = "localhost";
-        try {
-            hostAddress = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            log.warn("The host name could not be determined, using `localhost` as fallback");
-        }
-        log.info(
-            CRLFLogConverter.CRLF_SAFE_MARKER,
-            """
-
-            ----------------------------------------------------------
-            \tApplication '{}' is running! Access URLs:
-            \tLocal: \t\t{}://localhost:{}{}
-            \tExternal: \t{}://{}:{}{}
-            \tProfile(s): \t{}
-            ----------------------------------------------------------""",
-            applicationName,
-            protocol,
-            serverPort,
-            contextPath,
-            protocol,
-            hostAddress,
-            serverPort,
-            contextPath,
-            env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
-        );
-    }
 
 }
