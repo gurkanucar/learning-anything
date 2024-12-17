683a72a4e6d746a9de0f739d7cddd5ec5d8181de
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-12-17 22:45:52 +0300

    mail sending example updated

--- java/spring-boot-examples/aws-ses-mail-sending/src/main/java/org/gucardev/awssesmailsending/AwsSesMailSendingApplication.java
+++ java/spring-boot-examples/aws-ses-mail-sending/src/main/java/org/gucardev/awssesmailsending/AwsSesMailSendingApplication.java
@@ -2,7 +2,9 @@ package org.gucardev.awssesmailsending;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AwsSesMailSendingApplication {
 
--- java/spring-boot-examples/aws-ses-mail-sending/src/main/java/org/gucardev/awssesmailsending/EmailSenderService.java
+++ java/spring-boot-examples/aws-ses-mail-sending/src/main/java/org/gucardev/awssesmailsending/EmailSenderService.java
@@ -1,5 +1,6 @@
 package org.gucardev.awssesmailsending;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.gucardev.awssesmailsending.dto.HtmlEmailRequest;
@@ -7,35 +8,36 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import java.util.Map;
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class EmailSenderService {
 
-    private final SesEmailSender sesEmailSender;
-    private final SpringTemplateEngine templateEngine;
+  private final SesEmailSender sesEmailSender;
+  private final PostmarkEmailSender postmarkEmailSender;
+  private final SpringTemplateEngine templateEngine;
 
-    public void sendEmail(String to, String subject, String body) {
-        sesEmailSender.sendEmail(to, subject, body);
-    }
+  public void sendEmail(String to, String subject, String body) {
+    sesEmailSender.sendEmail(to, subject, body);
+    postmarkEmailSender.sendEmail(to, subject, body);
+  }
 
-    public void sendHtmlEmail(String to, String subject, String body) {
-        sesEmailSender.sendHtmlEmail(to, subject, body);
-    }
+  public void sendHtmlEmail(String to, String subject, String body) {
+    sesEmailSender.sendHtmlEmail(to, subject, body);
+    postmarkEmailSender.sendEmail(to, subject, body);
+  }
 
-    public void htmlSend(HtmlEmailRequest HTMLrequest, Map<String, Object> model) {
-        Context context = new Context();
-        context.setVariables(model);
+  public void htmlSend(HtmlEmailRequest HTMLrequest, Map<String, Object> model) {
+    Context context = new Context();
+    context.setVariables(model);
 
-        String templateName = HTMLrequest.templateName();
-        String to = HTMLrequest.to();
-        String subject = HTMLrequest.subject();
+    String templateName = HTMLrequest.templateName();
+    String to = HTMLrequest.to();
+    String subject = HTMLrequest.subject();
 
-        String processedHtml = templateEngine.process(templateName, context);
-        log.debug("Processed HTML: {}", processedHtml);
-        sendHtmlEmail(to, subject, processedHtml);
-    }
+    String processedHtml = templateEngine.process(templateName, context);
+    log.debug("Processed HTML: {}", processedHtml);
+    sendHtmlEmail(to, subject, processedHtml);
+  }
 
 }
