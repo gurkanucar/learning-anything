2a35fe755ccb3ec8f82c97c4ea2618b0e928b3f9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-12-23 21:15:42 +0300

    config updated

--- java/spring-boot-examples/utility/src/main/java/com/gucardev/utility/infrastructure/config/WebConfig.java
+++ java/spring-boot-examples/utility/src/main/java/com/gucardev/utility/infrastructure/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOriginPatterns("*") // Allow all origins
+            .allowedOrigins("http://localhost:3000", "https://example.com")  // list your domains here
+         //   .allowedOriginPatterns("*") // Allow all origins
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
         //  .allowedMethods("*")
         .allowedHeaders("*")
